a3b084d88663490280e27583122ad6d06f377dc4
anuragnegi000 <anurag.negi862@gmail.com>
2024-02-08 23:19:54 +0530

    added a component

---
+++ src/components/videoCard.tsx
@@ -0,0 +1,21 @@
+export function VideoCard(){
+    return <div>
+        <img src="/IMG_4134.JPG  "></img>
+        <div className="grid grid-cols-12">
+            <div className="col-span-1">
+            <img className={"rounded-full w-20 h-20"} src="/IMG_4134.JPG  "></img>
+        </div>
+        <div className="col-span-11 pl-5">
+            Anurag negi(4 years of life)
+            Btech computer science
+        </div>
+        </div>
+    </div> 
+
+} 
+
+function VideoCard2(){
+    return <div>
+
+    </div>
+}
